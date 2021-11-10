f9651c96b9e44e84d6f0a2393fd6947e3ab20d51
Johannes Siedersberger <johannes.siedersberger@gmail.com>
2021-11-10 15:44:36 +0100

    Load products from Húbspot, generate pdf, update them

--- server.js
+++ server.js
@@ -1,28 +1,101 @@
 const express = require('express');
 const app = express();
-const dotenv = require('dotenv');
 const jquersy = require('jquery');
 const axios = require('axios');
 const puppeteer = require('puppeteer');
-
-
-
-
-
-var templateUrl = 'https://hubspot-developers-1ty46i0-9489756.hs-sites.com/datenblatt-test/clicklux-rocket-30';
+const dotenv = require('dotenv');
+const request = require('request');
+const fs = require('fs');
+const FormData = require('form-data');
+const { response } = require('express');
+dotenv.config();
 
 // Laden des HTML Templates
 async function GetHTML() {
-    //const data = await axios.get(templateUrl);
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(templateUrl, {
-      waitUntil: 'networkidle2',
-    });
-    await page.pdf({ path: 'hn.pdf', format: 'a4' });
-  
-    await browser.close();
+    // Load Products from HubspotDB
+    await axios.get(`https://api.hubapi.com/cms/v3/hubdb/tables/5278217/rows?hapikey=${process.env.HUBSPOT_API_KEY}`).then( async (result) => {
+        // Get result
+        const productRows = result.data.results;
+
+        // Loop through all Products
+        for(var i=0; i< productRows.length; i++) {
+            // Get Path
+            const path = productRows[i].path;
+
+            // Check if path exists
+            if(path == null || path == undefined){
+              return;
+            }
+
+            // Build template url
+            var templateUrl = `http://lichtline-7712640.hs-sites.com/datenblatt/${path}`;
+            console.log(templateUrl);
+
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+          
+            await page.goto(templateUrl, {
+              waitUntil: 'networkidle2',
+            });
+
+            var fileName = `Datenblatt-Test-${path}.pdf`;
+            var pathToFile = `./files/Datenblatt-Test-${path}.pdf`
+            await page.pdf({ path: pathToFile, format: 'a4' });
+            await browser.close();
+
+            // Upload to Hubspot
+            var postUrl = `https://api.hubapi.com/filemanager/api/v3/files/upload?hapikey=${process.env.HUBSPOT_API_KEY}`;
+
+            // File Options
+            var fileOptions = {
+              access: 'PUBLIC_INDEXABLE',
+              ttl: 'P3M',
+              overwrite: true,
+              duplicateValidationStrategy: 'NONE',
+              duplicateValidationScope: 'ENTIRE_PORTAL'
+          };
+          
+          // Form Data
+          var formData = {
+              file: fs.createReadStream(pathToFile),
+              options: JSON.stringify(fileOptions),
+              folderPath: 'docs'
+          };
+          
+          // Send post request
+          request.post({
+              url: postUrl,
+              formData: formData
+          }, function optionalCallback(err, httpResponse, body){
+              //return console.log(body);
+          });
+          
+          // Friendly URL
+          var friendlyFileURL = `https://f.hubspotusercontent40.net/hubfs/7712640/docs/${fileName}`;
+          console.log(friendlyFileURL);
+
+          // Set URL in HubDB for product row
+          var urlForURLPost = `https://api.hubapi.com/hubdb/api/v2/tables/5278217/rows/59395405747/cells/4?hapikey=${process.env.HUBSPOT_API_KEY}`;
+         //axios.post(urlForURLPost, {"value" : "Updated Row Text"}).then((result) => {
+            axios.post(urlForURLPost, {"value" : "Updated Row Text"}, { params: {
+              "value" : "Updated Row Text"
+            }})
+            .then(response => console.log(response.data));
+          
+
+          
+        }
+        
+            
+          
+            
+        });
+
+       
+   
+
+
 
    
 
